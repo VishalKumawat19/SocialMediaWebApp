e93be2eb7920b914505ad86f7220f5155625268f
Vishal Kumawat <161027477+VishalKumawat19@users.noreply.github.com>
2024-08-14 12:18:22 +0530

    update cors configuration

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const errorHandler = require('./utils/errorHandler')
 //     next();
 // });
 const corsOptions = {
-    origin: "https://verqen-seven.vercel.app/",
+    origin: "https://verqen-seven.vercel.app",
     // optionsSuccessStatus: 200,
     credentials: true}
 
