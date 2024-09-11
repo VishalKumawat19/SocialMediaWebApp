498581613028fa9030bec2f9da84b968dda6bd6f
Vishal Kumawat <161027477+VishalKumawat19@users.noreply.github.com>
2024-09-12 01:29:59 +0530

    Update frontend code

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,3 @@
-
-// export const API_BASE_URL = 'https://api-verqen.vercel.app/api/v1'
-
 // export const API_BASE_URL = 'http://localhost:9000/api/v1'
 export const API_BASE_URL = 'https://api-verqen.vercel.app'
 // Auth API routes
